8ba45a614e533b903081f13a509c4c91936a29e7
devitfro <alinabogdanovaaaaa@gmail.com>
2026-08-12 18:32:03 +0300

    feat(web): add sidebar with filters, add main content

--- apps/web/src/components/ui/search/Search.jsx
+++ apps/web/src/components/ui/search/Search.jsx
@@ -84,11 +84,12 @@ const SearchWrapper = styled.div`
 `;
 
 
-export const Search = ({ size='medium', type='text', placeholder = 'Пошук курсів...', onKeyDown, onChange, required} ) => (
+export const Search = ({ size='medium', type='text', value='', placeholder = 'Пошук курсів...', onKeyDown, onChange, required} ) => (
   <SearchWrapper $size={size}>
     <img src={searchIcon} alt='search icon'/>
     <input type={type}
            placeholder={placeholder}
+           value={value}
            onKeyDown={() => {}}
            onChange={onChange}
            required={required}
