f486224e005d24a87398d5a267be9a5f4ced52a8
devitfro <alinabogdanovaaaaa@gmail.com>
2026-09-02 13:24:16 +0300

    update course page

--- apps/web/src/pages/course/Course.jsx
+++ apps/web/src/pages/course/Course.jsx
@@ -21,8 +21,7 @@ import checkBallIcon from '../../assets/icons/check-ball.svg';
 import Button from '../../components/ui/buttons/Button';
 import { Title } from '../../components/ui/title/Title';
 import { CardsList } from '../../components/ui/cards-list/CardsList.jsx';
-
-import { CourseAuthorCard } from './CourseAuthorСard.jsx';
+import { CourseAuthorCard } from './CourseAuthorCard.jsx';
 import { OpenMore } from '../../components/ui/openmore/OpenMore.jsx';
 import LessonModule from '../../components/ui/module/LessonModule.jsx';
 
